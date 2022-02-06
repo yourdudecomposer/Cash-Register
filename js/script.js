b06b467a2ebcd082a2cf21f1d781820815f4bc06
Filipp Goriainov <barrios1991phil@gmail.com>
2022-02-07 00:59:19 +0400

    little making js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,21 @@
 btnCount.addEventListener('click', count);
 
-function count(){
+function count() {
     let cid = makeArrayFromCid();
     let result = checkCashRegister(price, cash, cid);
-console.log('result');
-console.log(result);
+    switch (result.status) {
+        case 'OPEN':
+            fillTable(result.change);
+            break;
+        case 'CLOSED':
+            fillTableAndClose(result.change);
+            break;
+        case 'INSUFFICIENT_FUNDS':
+            notMoney();
+            break;
+        default: console.log('something wrong in switch case');
+            break;
+    }
+    console.log('result');
+    console.log(result);
 }
